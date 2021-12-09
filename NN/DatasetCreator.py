dadd18beabad6fe7332c14f91283cb3e59a6e035
daviholy <david.holy225@gmail.com>
2021-12-09 01:30:07 +0100

    fixed model, now should interfere normally

---
+++ NN/DatasetCreator.py
@@ -0,0 +1,45 @@
+from torch.utils.data import Dataset
+from pathlib import Path
+from torchvision.io import ImageReadMode
+from torchvision.io import read_image
+from torch import tensor
+from os import path
+import json
+
+
+class DatasetCreator(Dataset):
+    def __init__(self, dest_photos="dataset/photos",dest_labels="dataset/labels", transform=None, target_transform=None):
+
+        if not path.isdir(Path(dest_photos)):
+            exit("Not a valid image directory")
+        if not path.isdir(Path(dest_labels)):
+            exit("Not a valid label directory")   
+
+        self.labels = sorted(Path(dest_labels).glob("*.json"))
+        self.transform = transform
+        self.target_transform = target_transform
+        self.photos = dest_photos
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        jfile = None
+        with open(self.labels[idx], 'r') as j:
+            jfile = json.load(j)
+        img = read_image(
+            f'{self.photos}/{jfile["task"]["data"]["image"].split("/")[-1]}',
+            ImageReadMode.RGB)
+        if len(jfile["result"]) != 0:
+            label = jfile["result"][0]["value"]["choices"][0]
+        else:
+            return img, []
+        if self.transform:
+            img = self.transform(img)
+        if self.target_transform:
+            label = self.target_transform(label)
+        if label == 'car':  # TODO: tahle transformace by se asi mela predelat (neni moc hezka :D ale zatim funguje)
+            label = 1.0
+        else:
+            label = 0.0
+        return img.float(), tensor(label)
--- trainingModel.py
+++ trainingModel.py
@@ -1,19 +1,16 @@
-import os.path
+
 import torch
-from sys import exit
-from pathlib import Path
-from torchvision.io import read_image
-from torchvision.io import ImageReadMode
 from torchvision import transforms
 from torch import nn
-from torch import tensor
+import torch.nn.functional as F
 from torch import zeros
-from torch.utils.data import Dataset
 from torch.utils.data.sampler import RandomSampler
 from torch.utils.data.dataloader import DataLoader
-import json
+from NN import DatasetCreator
 import argparse
 import matplotlib.pyplot as plt
+from NN import DatasetCreator
+from NN import NeuralNetwork
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--directory', type=str, default='./dataset/split',
@@ -28,64 +25,6 @@ testing_dir = {'labels': f'{args.directory}/testing/labels',
                'photos': f'{args.directory}/testing/photos'}
 
 dest_dirs = {'training': training_dir, 'testing': testing_dir}
-
-
-class DatasetCreator(Dataset):
-    def __init__(self, dataset_type, annotations_file=None, transform=None, target_transform=None):
-
-        if not os.path.isdir(Path(dest_dirs[dataset_type]['labels'])):
-            exit("Not a valid directory")
-
-        self.labels = sorted(Path(dest_dirs[dataset_type]['labels']).glob("*.json"))
-        self.dataset_type = dataset_type
-        self.transform = transform
-        self.target_transform = target_transform
-
-    def __len__(self):
-        return len(self.labels)
-
-    def __getitem__(self, idx):
-        jfile = None
-        with open(self.labels[idx], 'r') as j:
-            jfile = json.load(j)
-        img = read_image(
-            f'{dest_dirs[self.dataset_type]["photos"]}/{jfile["task"]["data"]["image"].split("/")[-1]}',
-            ImageReadMode.RGB)
-        if len(jfile["result"]) != 0:
-            label = jfile["result"][0]["value"]["choices"][0]
-        else:
-            return img, []
-        if self.transform:
-            img = self.transform(img)
-        if self.target_transform:
-            label = self.target_transform(label)
-        return img.float(), tensor(1) #TODO: implement transformation text to tensor (now its only returning 1 always)
-
-
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.layer_input = nn.Sequential(nn.AdaptiveMaxPool2d(120))
-        self.layer1 = nn.Sequential(nn.Conv2d(1, 32, 1), nn.Conv2d(
-            32, 32, 3), nn.BatchNorm2d(1), nn.ReLU(), nn.MaxPool2d(3))
-        self.layer2 = nn.Sequential(nn.Conv2d(32, 64, 1), nn.Conv2d(
-            64, 64, 3), nn.BatchNorm2d(32), nn.ReLU(), nn.MaxPool2d(3))
-        self.layer3 = nn.Sequential(nn.Conv2d(64, 128, 1), nn.Conv2d(
-            128, 128, 3), nn.BatchNorm2d(128), nn.ReLU(), nn.MaxPool2d(3))
-        self.layer4 = nn.Sequential(nn.Conv2d(128, 256, 1), nn.Conv2d(
-            256, 256, 3), nn.BatchNorm2d(256), nn.ReLU(), nn.Flatten())
-        self.layer_output = nn.Sequential(
-            nn.Linear(1024, 512), nn.BatchNorm2d(256), nn.Sigmoid())
-
-    def forward(self, x):
-        x = self.layer_input(x)
-        x = self.conv(x)
-        x = self.conv2(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        return self.layer_output(x)
 
 
 def _collate_fn_pad(batch):
@@ -116,14 +55,12 @@ def _collate_fn_pad(batch):
 
     return padded_imgs, labels
 
-
 def debug(func):
     def inner(*arg):
         if args.DEBUG:
             func(*arg)
 
     return inner
-
 
 @debug
 def test_data_loaders(train_loader, test_loader):
@@ -153,13 +90,13 @@ def test_data_loaders(train_loader, test_loader):
     plt.imshow(img.squeeze(), cmap='gray')
     plt.show()
 
-
 if __name__ == "__main__":
     batch_size = 5
 
-    train_data = DatasetCreator('training', transform=nn.Sequential(
+
+    train_data = DatasetCreator(training_dir['photos'],training_dir['labels'], transform=nn.Sequential(
         transforms.Grayscale(), transforms.RandomEqualize(p=1)))
-    test_data = DatasetCreator('testing', transform=nn.Sequential(
+    test_data = DatasetCreator(testing_dir['photos'],testing_dir['labels'], transform=nn.Sequential(
         transforms.Grayscale(), transforms.RandomEqualize(p=1)))
 
     train_loader = DataLoader(train_data, batch_size=batch_size, sampler=RandomSampler(data_source=train_data),
@@ -169,7 +106,18 @@ if __name__ == "__main__":
 
     test_data_loaders(train_loader, test_loader)
 
+
+    classes = ('empty', 'car')
+
+    model = NeuralNetwork().to(device)
+
+    model.load_state_dict(torch.load("../model/model2.pth"))  # Toto je nacitani jiz existujiciho modelu
+    model.eval()
+
+    model.evaluate_model(test_loader)
+    # train_model()
     training = NeuralNetwork().to("cpu")
 
+    #1 epoch
     for (data, label) in train_loader:
-        training(data)
+        print(training(data))
