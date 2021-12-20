7c578d1449e88c9da41c26a9312a3669b327d1a0
Frantisek Cmuchat <fanda.cmuchar@gmail.com>
2021-12-20 13:25:16 +0100

    added arguments for script manipulation

--- trainingModel.py
+++ trainingModel.py
@@ -5,6 +5,8 @@ from torch import zeros
 from torch.utils.data.sampler import RandomSampler
 from torch.utils.data.dataloader import DataLoader
 import argparse
+import os
+from sys import exit
 import matplotlib.pyplot as plt
 from NN.DatasetCreator import DatasetCreator
 from NN.NeuralNetwork import NeuralNetwork
@@ -12,6 +14,12 @@ from NN.NeuralNetwork import NeuralNetwork
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--directory', type=str, default='./dataset/split',
                     help='directory where the dataset is stored')
+parser.add_argument('-eval', '--eval_mode', type=bool, default=True,
+                    help='decides if model will be trained or evaluated')
+parser.add_argument('-em', '--existing_model_path', type=str, default='',
+                    help='path to existing model that you wish to load, if empty model will not be loaded')
+parser.add_argument('-sm', '--save_model_path', type=str, default='',
+                    help='path with name of the model that you wish to save, if empty model will not be saved')
 parser.add_argument('--DEBUG', type=bool, default=False,
                     help='decides if script will run in debug mode (prints to stdout)')
 args = parser.parse_args()
@@ -114,14 +122,26 @@ if __name__ == "__main__":
 
     classes = ('empty', 'car')
 
-    # model = NeuralNetwork(classes, device).to(device)
-
-    # model.load_state_dict(torch.load("../model/model_b64_e25.pth"))  # Toto je nacitani jiz existujiciho modelu
-    # model.eval()
-
     test_data_loaders(train_loader, test_loader)
-    # model.train_model(train_loader, num_epochs, learning_rate)
-    # torch.save(model.state_dict(), "../model/model_b64_e25r0_01.pth")
 
-    # model.evaluate_model(test_loader)
+    model = NeuralNetwork(classes, device).to(device)
+
+    # Check input arguments
+    if len(args.existing_model_path) > 0:
+        if os.path.isfile(args.existing_model_path):
+            exit("invalid path to model you wish to load")
+        model.load_state_dict(torch.load(args.existing_model_path))
+
+    if len(args.save_model_path) > 0:
+        if not os.path.isdir(args.save_model_path):
+            exit("invalid path to save model")
+
+    if args.eval_mode:
+        model.eval()
+        model.evaluate_model(test_loader)
+    else:
+        model.train_model(train_loader, num_epochs, learning_rate)
+        if len(args.save_model_path) > 0:
+            torch.save(model.state_dict(), args.save_model_path)
+
 
